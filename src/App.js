da41a438f2dc2ceb97f5772965f0213b95b844d1
kirby36 <aydan.h.y@gmail.com>
2018-04-29 19:57:51 -0700

    edited

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. HELLO
+          To get started, edit <code>src/App.js</code> and save to reload. HELLOI PUT SOE TEXT
         </p>
         <Weather />
       </div>
